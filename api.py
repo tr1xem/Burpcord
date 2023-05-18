e26f9daef6794d6383be756f3043ffad00dcd08d
AntiVlad <vladnyash@gmail.com>
2023-05-18 08:33:45 +0100

    Api change

--- api.py
+++ api.py
@@ -1,26 +1,11 @@
-import Burpcord.rpc as rpc
+from pypresence import Presence
 import time
 
-print("Demo for python-discord-rpc")
-client_id = '993442275533271050' #Your application's client ID as a string. (This isn't a real client ID)
-rpc_obj = rpc.DiscordIpcClient.for_platform(client_id) #Send the client ID to the rpc module
-print("RPC connection successful.")
+client_id = '993442275533271050'  
+RPC = Presence(client_id)  
+RPC.connect() 
 
-time.sleep(5)
-start_time = time.time()
-while True:
-    activity = {
-            "state": "Scanning for Vulnerabilities",
-            "details": "",
-            "timestamps": {
-                "start": start_time
-            },
-            "assets": {
-                "small_text": "",
-                "small_image": "cover",
-                "large_text": "",
-                "large_image": "burp"
-            }
-        }
-    rpc_obj.set_activity(activity)
-    time.sleep(30)
+print(RPC.update(state = "Scanning for Vulnerabilities",large_image = "burp",largeImageText = "efeefe",smallImageKey = "cover",smallImageText = "eeeee",))  # Set the presence
+
+while True:  
+    time.sleep(15) 
