ce1d16181c0e4b4fc72e94ffd12e5cf60e3bc7ff
tr1x_em <kmtechnical2006@gmail.com>
2024-03-16 21:09:27 +0530

    modified:   Readme.md 	modified:   api.py 	deleted:    dist/api.exe

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 from pypresence import Presence
 import time
-
+import datetime
 client_id = '993442275533271050'  
 RPC = Presence(client_id)  
 RPC.connect() 
 
-print(RPC.update(state = "Scanning for Vulnerabilities",start= time.time(),large_image = "burp",large_text = "Scanning",small_image = "cover",small_text = "Burp Suite")) 
+print(RPC.update(state = "Scanning for Vulnerabilities",start= datetime.datetime.now().timestamp()  ,large_image = "burp",large_text = "Scanning",small_image = "cover",small_text = "BurpSuite")) 
 print("Running")
 while True:  
     time.sleep(15) 
